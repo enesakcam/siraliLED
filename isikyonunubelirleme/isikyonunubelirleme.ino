580200d78a22be5c0e4e7da7ed9184a478887102
ENES AKÇAM <enesakcm9@gmail.com>
2022-10-24 10:29:19 +0300

    program kodları yazıldı

---
+++ isikyonunubelirleme/isikyonunubelirleme.ino
@@ -0,0 +1,16 @@
+int i;
+void setup() {
+ for(i=2;i<=6;i++) pinMode(i, OUTPUT);
+ pinMode(11, INPUT);
+}
+void loop() {
+
+if (digitalRead(11)==1){
+for(i=2;i<=6;i++){ digitalWrite(i, 1); delay(250); }
+for(i=2;i<=6;i++){ digitalWrite(i, 0); delay(250); }
+ }
+if (digitalRead(11)==0){
+for(i=6;i>=2;i--){ digitalWrite(i, 1); delay(250); }
+for(i=6;i>=2;i--){ digitalWrite(i, 0); delay(250); }
+ }
+}
